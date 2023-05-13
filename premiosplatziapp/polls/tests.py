8e4832339dd0e05db88e0be9219be5d7b95e5155
Franciscotor1 <francisco.jta01@gmail.com>
2023-05-13 11:37:38 -0600

    commit

--- premiosplatziapp/polls/tests.py
+++ premiosplatziapp/polls/tests.py
@@ -1,3 +1,42 @@
+import datetime
 from django.test import TestCase
+from django.urls.base import reverse
+from django.utils import timezone
 
-# Create your tests here.
+from .models import Question
+
+# vas a testear modelos y vistas
+class QuestionModelTEst(TestCase):
+    
+    def test_was_published_recently_with_future_questions(self):
+        """was_published_recently returns False for quesitons whose pub_date is in the future"""
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(question_text="Quien es el mejor Director de Platzi", pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
+
+def create_question(question_text, days):
+    """
+    Create a question with the given "question_text", and published the given number
+    of days offset to now (negative for questios published in the past,
+    positive for questions thaat have yet to be published)
+    """
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
+        
+class QuestionIndexViewTest(TestCase):
+    def test_no_questions(self):
+        """If no question exist, an appropiate message is displayed"""
+        response = self.client.get(reverse("polls:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context["latest_question_list"], [])
+
+    def test_future_question(self):
+        """
+        Questions with a pub_date in the future aren't displayed on the index page.
+        """
+        create_question("Future question", days=30)
+        response = self.client.get(reverse("polls:index"))
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context["latest_question_list"], [])  
--- premiosplatziapp/polls/urls.py
+++ premiosplatziapp/polls/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from . import views
 
+app_name = "polls"
 urlpatterns = [
-    path("", views.index, name='index'),
+    path("", views.IndexView.as_view(), name='index'),
+    # acceder con: /polls/5/
+    path("<int:pk>/detail/estaeslamejorpagina", views.DetailView.as_view(), name="detail"),
+    # acceder con: /polls/5/results
+    path("<int:pk>/results/", views.ResultView.as_view(), name="results"),
+     # acceder con: /polls/5/vote
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
